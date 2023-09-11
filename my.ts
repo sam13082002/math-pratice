20ecab3e77c0defb6d4ad305e19b4d6415727329
Sam <hangho.dev@gmail.com>
2023-09-11 14:50:52 +0700

    hinh-vuong

--- my.ts
+++ my.ts
@@ -1,5 +1,6 @@
-let a = "dien tich hinh chu nhat";
-let b = "chu vi hinh chu nhat";
-let dai = 3;
-let rong = 4;
-console.log(b, dai * rong);
+let c = "dien tich hinh vuong";
+let d = "chu vi hinh vuong";
+let canh = 3;
+let a = 4;
+
+console.log(d, canh * a);
